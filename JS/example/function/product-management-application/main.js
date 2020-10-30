4adddd4097e5f495d5820590ff83607e8e5c6c5c
Domia <39hson@gmail.com>
2020-10-30 23:04:59 +0700

    Doing

--- JS/example/function/product-management-application/main.js
+++ JS/example/function/product-management-application/main.js
@@ -1,12 +1,13 @@
 //declare HTML elements
 let addInput = document.getElementById("input-new-product");
 let addBtn = document.getElementById("add-btn");
+let prodctNumbẻ
 
 let productList = ["Vsmart Live", "Samsung A50"];
 
 //
 function init(){
-    
+    let 
 }
 
 // khi ấn nút add thì thêm giá trị 
@@ -20,3 +21,5 @@ function edit(){
 function del(){
 
 }
+
+init();
