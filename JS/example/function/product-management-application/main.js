89f811840718dc0b52fe9c0d1618f51312ce8aab
Domia <39hson@gmail.com>
2020-10-31 11:43:33 +0700

    Done init, add function

--- JS/example/function/product-management-application/main.js
+++ JS/example/function/product-management-application/main.js
@@ -1,24 +1,45 @@
 //declare HTML elements
 let addInput = document.getElementById("input-new-product");
 let addBtn = document.getElementById("add-btn");
-let prodctNumbẻ
+
+addBtn.addEventListener("click", add);
 
 let productList = ["Vsmart Live", "Samsung A50"];
 
+
 //
-function init(){
-    let 
+function init() {
+    let tableBody = document.getElementById("table-body");
+    let insertHTML = "";
+    for (let i = 0; i < productList.length; i++) {
+        insertHTML += "<tr>"
+        insertHTML += `<td>${productList[i]}</td> `;
+        insertHTML += `<td><input id="edit-input" class="btn btn-primary mb-2" type="button" value="Edit"></td>`;
+        insertHTML += `<td><input id="del-input" class="btn btn-primary mb-2" type="button" value="Del"></td>`;
+        insertHTML += "</tr>"
+
+    }
+    tableBody.innerHTML = insertHTML;
+    displayProductNumber();
 }
 
-// khi ấn nút add thì thêm giá trị 
-function add(){
+// khi ấn nút add thì thêm giá trị từ input trong HTML để đưa vào mảng productList
+function add() {
+    productList.push(addInput.value);
+    init();
+    addInput.value = "";
 }
 
-function edit(){
-
+//hiển thị số lượng sản phẩm
+function displayProductNumber(){
+    let productNumber = document.getElementById("product-number");
+    productNumber.innerHTML = productList.length;
+}
+function edit() {
+    
 }
 
-function del(){
+function del() {
 
 }
 
