99e5a9b0b0532ceed501e1f043e9ab8f6fb38b59
Domia <39hson@gmail.com>
2020-11-01 13:17:10 +0700

    Done edit function

--- JS/example/function/product-management-application/main.js
+++ JS/example/function/product-management-application/main.js
@@ -6,19 +6,20 @@ addBtn.addEventListener("click", add);
 
 let productList = ["Vsmart Live", "Samsung A50"];
 
+let editAvailable = true;
 
 //
 function init() {
     let tableBody = document.getElementById("table-body");
     let insertHTML = "";
     for (let i = 0; i < productList.length; i++) {
-        insertHTML += "<tr>"
-        insertHTML += `<td>${productList[i]}</td> `;
-        insertHTML += `<td><input id="edit-input" class="btn btn-primary mb-2" type="button" value="Edit"></td>`;
+        insertHTML += `<tr>`
+        insertHTML += `<td><input id="no-${i+1}" disabled type="text" value="${productList[i]}"></td> `;
+        insertHTML += `<td><input id="edit-input" onclick="edit(event, this)" class="btn btn-primary mb-2" type="button" value="Edit"></td>`;
         insertHTML += `<td><input id="del-input" class="btn btn-primary mb-2" type="button" value="Del"></td>`;
         insertHTML += "</tr>"
-
     }
+
     tableBody.innerHTML = insertHTML;
     displayProductNumber();
 }
@@ -31,12 +32,24 @@ function add() {
 }
 
 //hiển thị số lượng sản phẩm
-function displayProductNumber(){
+function displayProductNumber() {
     let productNumber = document.getElementById("product-number");
-    productNumber.innerHTML = productList.length;
+    productNumber.innerHTML = "Số lượng sản phẩm: " + productList.length;
 }
-function edit() {
-    
+
+function edit(e, t) {
+    if (editAvailable) {
+        e.path[2].getElementsByTagName("input")[0].disabled = false;
+        t.value = "Save";
+        editAvailable = false;
+    } else {
+        e.path[2].getElementsByTagName("input")[0].disabled = true;
+
+        productList[parseInt(e.path[2].getElementsByTagName("input")[0].id.split("-")[1] - 1)] = e.path[2].getElementsByTagName("input")[0].value;
+        t.value = "Edit";
+        // console.log(productList);
+        editAvailable = true;
+    }
 }
 
 function del() {
